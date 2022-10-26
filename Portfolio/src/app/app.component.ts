da77e6c56da282e150dd86fe45ca327dda8cfa36
German Lopez <gerleven@gmail.com>
2022-10-26 11:53:44 -0300

    first deploy done

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Portfolio';
-  // ng build --target=production --environment=prod\ --base-href="https://gerleven.github.io/Portfolio/"
   // ng add angular-cli-ghpages
+  //ng deploy --base-href=/<RepoName, por ejemplo “Portfolio”>/
 }
